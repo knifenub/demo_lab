a90479897d85c72aa36df25cff20d76339d17442
knifenub <naif050923@gmail.com>
2026-07-22 12:49:16 +0600

    Displayed student ranking in main

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "course.h"
 #include "courseResult.h"
 #include "student.h"
@@ -10,9 +11,10 @@ int main()
     Course cse4108 =
         createCourse("CSE 4108", "Structured Programming I Lab", 1.5, 1);
 
-    Student students[2] = {
+    Student students[3] = {
         createStudent("240041001", "Alice"),
-        createStudent("240041002", "Bob")
+        createStudent("240041002", "Bob"),
+        createStudent("240041003", "Carol")
     };
 
     addCourseResultToStudent(
@@ -35,8 +37,21 @@ int main()
         createCompletedCourseResult(&cse4108, 123)
     );
 
-    for (int i = 0; i < 2; i++)
+    addCourseResultToStudent(
+        &students[2],
+        createCompletedCourseResult(&cse4107, 273)
+    );
+
+    addCourseResultToStudent(
+        &students[2],
+        createCompletedCourseResult(&cse4108, 130.5)
+    );
+
+    sortStudentsByCGPA(students, 3);
+
+    for (int i = 0; i < 3; i++)
     {
+        printf("%d. ", i + 1);
         viewStudent(students[i]);
     }
 
